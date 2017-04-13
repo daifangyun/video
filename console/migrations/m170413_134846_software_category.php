c26a6ab2e369f235f38ad3f1323bec76095063e1
dfy <490286352@qq.com>
2017-04-13 22:32:25 +0800

    2017年4月13日22:32:24

--- console/migrations/m170413_134846_software_category.php
+++ console/migrations/m170413_134846_software_category.php
@@ -2,9 +2,9 @@
 
 use yii\db\Migration;
 
-class m170412_032015_tag extends Migration
+class m170413_134846_software_category extends Migration
 {
-    private $tableName = '{{%tag}}';
+    private $tableName = '{{%software_category}}';
 
     public function up()
     {
@@ -16,9 +16,8 @@ class m170412_032015_tag extends Migration
         $this->createTable($this->tableName, [
             'id' => $this->primaryKey()->comment('自增id'),
 
-            'name' => $this->string(50)->notNull()->defaultValue('')->comment('标签名称'),
+            'name' => $this->string(50)->notNull()->defaultValue('')->comment('分类名称'),
             'pid' => $this->integer(11)->defaultValue(0)->comment('0:顶级分类 || > 0 对应id列的值'),
-            'cid' => $this->integer(11)->notNull()->defaultValue(0)->comment('所属分类,分类表中的顶级分类ID'),
 
             'sort' => $this->integer(4)->defaultValue(0)->comment('排序,按照由大到小的顺序排,大的在前边,小的在后边'),
             'status' => $this->smallInteger(1)->defaultValue(1)->comment('是否有效,1:有效,0:无效,-1:永久删除'),
@@ -28,22 +27,8 @@ class m170412_032015_tag extends Migration
 
         //插入一条数据
         $this->insert($this->tableName, [
-            'id' => 1,
-            'name' => '内容',
+            'name' => '	C4D',
             'pid' => 0,
-            'cid' => 1,
-            'sort' => 1,
-            'status' => 1,
-            'created_at' => time(),
-            'updated_at' => time(),
-        ]);
-
-        //插入一条数据
-        $this->insert($this->tableName, [
-            'id' => 2,
-            'name' => '风格',
-            'pid' => 0,
-            'cid' => 1,
             'sort' => 1,
             'status' => 1,
             'created_at' => time(),
@@ -51,33 +36,36 @@ class m170412_032015_tag extends Migration
         ]);
         //插入一条数据
         $this->insert($this->tableName, [
-            'id' => 3,
-            'name' => '软件',
-            'pid' => 0,
-            'cid' => 1,
-            'sort' => 1,
+            'name' => '	R17',
+            'pid' => 1,
+            'sort' => 2,
             'status' => 1,
             'created_at' => time(),
             'updated_at' => time(),
         ]);
         //插入一条数据
         $this->insert($this->tableName, [
-            'id' => 4,
-            'name' => 'C4D',
+            'name' => 'Adobe PhotoShop',
+            'pid' => 0,
+            'sort' => 3,
+            'status' => 1,
+            'created_at' => time(),
+            'updated_at' => time(),
+        ]);
+        //插入一条数据
+        $this->insert($this->tableName, [
+            'name' => 'cs2',
             'pid' => 3,
-            'cid' => 1,
-            'sort' => 1,
+            'sort' => 3,
             'status' => 1,
             'created_at' => time(),
             'updated_at' => time(),
         ]);
         //插入一条数据
         $this->insert($this->tableName, [
-            'id' => 5,
-            'name' => 'AI',
+            'name' => 'cs3',
             'pid' => 3,
-            'cid' => 1,
-            'sort' => 1,
+            'sort' => 3,
             'status' => 1,
             'created_at' => time(),
             'updated_at' => time(),
